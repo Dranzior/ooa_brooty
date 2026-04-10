a88bda02e6babfa529aec5b92eeca2bc134d5bd7
Dranzior <>
2026-04-09 23:08:27 -0400

    Updated location and item ID to (hopefully) all the new value

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -2,121 +2,149 @@ CURRENT_ITEM = nil
 
 BASE = 27022002000
 
-ITEM_MAPPING = {
+ITEM_LISTING = {
     -- INVENTORY
-    [BASE + 0] = {"shield", "progressive"},
-    [BASE + 1] = {"bombs", "progressive"},
-    [BASE + 2] = {"swordupgrade", "progressive"},
-    [BASE + 3] = {"boomerang", "progressive"},
-    [BASE + 4] = {"harpupgrade", "progressive"},
-    [BASE + 5] = {"hookupgrade", "progressive"},
-    [BASE + 6] = {"cane", "toggle"},
-    [BASE + 7] = {"bigsword", "toggle"},
-    [BASE + 8] = {"flute", "toggle"},
-    [BASE + 9] = {"flute", "toggle"},
-    [BASE + 10] = {"flute", "toggle"},
-    [BASE + 11] = {"shooter", "toggle"},
-    [BASE + 12] = {"shovel", "toggle"},
-    [BASE + 13] = {"liftupgrade", "progressive"},
-    [BASE + 14] = {"feather", "progressive"},
-    [BASE + 15] = {"satchel", "progressive"},
-    [BASE + 16] = {"emberseeds", "toggle"},
-    [BASE + 17] = {"scentseeds", "toggle"},
-    [BASE + 18] = {"pegasusseeds", "toggle"},
-    [BASE + 19] = {"galeseeds", "toggle"},
-    [BASE + 20] = {"mysteryseeds", "toggle"},
+    {"shield", "progressive"},
+    {"bombs", "progressive"},
+    {"swordupgrade", "progressive"},
+    {"boomerang", "progressive"},
+    {"harpupgrade", "progressive"},
+    {"hookupgrade", "progressive"},
+    {"cane", "toggle"},
+    {"bigsword", "toggle"},
+    {"flute", "toggle"},
+    {"flute", "toggle"},
+    {"flute", "toggle"},
+    {"shooter", "toggle"},
+    {"shovel", "toggle"},
+    {"liftupgrade", "progressive"},
+    {"feather", "progressive"},
+    {"satchel", "progressive"},
+    {"emberseeds", "toggle"},
+    {"scentseeds", "toggle"},
+    {"pegasusseeds", "toggle"},
+    {"galeseeds", "toggle"},
+    {"mysteryseeds", "toggle"},
     -- RUPEES
-    -- [BASE + 21] = {"rupees", "consumable"},  --   1 Rupee
-    [BASE + 22] = {"rupees", "consumable"},     --   5 Rupees
-    [BASE + 23] = {"rupees", "consumable", 2},  --  10 Rupees
-    [BASE + 24] = {"rupees", "consumable", 4},  --  20 Rupees
-    [BASE + 25] = {"rupees", "consumable", 6},  --  30 Rupees
-    [BASE + 26] = {"rupees", "consumable", 10}, --  50 Rupees
-    [BASE + 27] = {"rupees", "consumable", 20}, -- 100 Rupees
-    [BASE + 28] = {"rupees", "consumable", 40}, -- 200 Rupees
+    1,
+    {"rupees", "consumable"},     --   5 Rupees
+    {"rupees", "consumable", 2},  --  10 Rupees
+    {"rupees", "consumable", 4},  --  20 Rupees
+    {"rupees", "consumable", 6},  --  30 Rupees
+    {"rupees", "consumable", 10}, --  50 Rupees
+    {"rupees", "consumable", 20}, -- 100 Rupees
+    {"rupees", "consumable", 40}, -- 200 Rupees
+    2,
     -- PASSIVE
-    [BASE + 31] = {"swimupgrade", "progressive"},
-    [BASE + 33] = {"potion", "toggle"},
+    {"swimupgrade", "progressive"},
+    1,
+    {"potion", "toggle"},
     -- SMALL KEYS
-    [BASE + 34] = {"pathkey", "toggle"},
-    [BASE + 35] = {"d1sk", "consumable"},
-    [BASE + 36] = {"d2sk", "consumable"},
-    [BASE + 37] = {"d3sk", "consumable"},
-    [BASE + 38] = {"d4sk", "consumable"},
-    [BASE + 39] = {"d5sk", "consumable"},
-    [BASE + 40] = {"d6_1sk", "consumable"},
-    [BASE + 41] = {"d6_2sk", "consumable"},
-    [BASE + 42] = {"d7sk", "consumable"},
-    [BASE + 43] = {"d8sk", "consumable"},
+    {"pathkey", "toggle"},
+    {"d1sk", "consumable"},
+    {"d2sk", "consumable"},
+    {"d3sk", "consumable"},
+    {"d4sk", "consumable"},
+    {"d5sk", "consumable"},
+    {"d6_1sk", "consumable"},
+    {"d6_2sk", "consumable"},
+    {"d7sk", "consumable"},
+    {"d8sk", "consumable"},
+    1, --Linked Hero Cave
     -- MASTER KEYS
-    [BASE + 44] = {"pathkey", "toggle"},
-    [BASE + 45] = {"d1mk", "toggle"},
-    [BASE + 46] = {"d2mk", "toggle"},
-    [BASE + 47] = {"d3mk", "toggle"},
-    [BASE + 48] = {"d4mk", "toggle"},
-    [BASE + 49] = {"d5mk", "toggle"},
-    [BASE + 50] = {"d6_1mk", "toggle"},
-    [BASE + 51] = {"d6_2mk", "toggle"},
-    [BASE + 52] = {"d7mk", "toggle"},
-    [BASE + 53] = {"d8mk", "toggle"},
+    {"pathkey", "toggle"},
+    {"d1mk", "toggle"},
+    {"d2mk", "toggle"},
+    {"d3mk", "toggle"},
+    {"d4mk", "toggle"},
+    {"d5mk", "toggle"},
+    {"d6_1mk", "toggle"},
+    {"d6_2mk", "toggle"},
+    {"d7mk", "toggle"},
+    {"d8mk", "toggle"},
+    1, --Linked Hero Cave
     -- BOSS KEYS
-    [BASE + 54] = {"d1bk", "toggle"},
-    [BASE + 55] = {"d2bk", "toggle"},
-    [BASE + 56] = {"d3bk", "toggle"},
-    [BASE + 57] = {"d4bk", "toggle"},
-    [BASE + 58] = {"d5bk", "toggle"},
-    [BASE + 59] = {"d6bk", "toggle"},
-    [BASE + 60] = {"d7bk", "toggle"},
-    [BASE + 61] = {"d8bk", "toggle"},
+    {"d1bk", "toggle"},
+    {"d2bk", "toggle"},
+    {"d3bk", "toggle"},
+    {"d4bk", "toggle"},
+    {"d5bk", "toggle"},
+    {"d6bk", "toggle"},
+    {"d7bk", "toggle"},
+    {"d8bk", "toggle"},
+    18, -- Map and Compass
+    1,
     -- TRADE
-    [BASE + 81] = {"poeclock", "toggle"},
-    [BASE + 82] = {"stationary", "toggle"},
-    [BASE + 83] = {"stinkbag", "toggle"},
-    [BASE + 84] = {"tastymeat", "toggle"},
-    [BASE + 85] = {"doggiemask", "toggle"},
-    [BASE + 86] = {"dumbbell", "toggle"},
-    [BASE + 87] = {"mustache", "toggle"},
-    [BASE + 88] = {"funnyjoke", "toggle"},
-    [BASE + 89] = {"touchingbook", "toggle"},
-    [BASE + 90] = {"magicoar", "toggle"},
-    [BASE + 91] = {"seaukulele", "toggle"},
-    [BASE + 92] = {"brokensword", "toggle"},
+    {"poeclock", "toggle"},
+    {"stationary", "toggle"},
+    {"stinkbag", "toggle"},
+    {"tastymeat", "toggle"},
+    {"doggiemask", "toggle"},
+    {"dumbbell", "toggle"},
+    {"mustache", "toggle"},
+    {"funnyjoke", "toggle"},
+    {"touchingbook", "toggle"},
+    {"magicoar", "toggle"},
+    {"seaukulele", "toggle"},
+    {"brokensword", "toggle"},
     -- RANDOM ITEMS
-    [BASE + 93] = {"bombflower", "toggle"},
-    [BASE + 94] = {"book", "toggle"},
-    [BASE + 95] = {"emblem", "toggle"},
-    [BASE + 96] = {"cheval", "toggle"},
-    [BASE + 97] = {"crownkey", "toggle"},
-    [BASE + 98] = {"powder", "toggle"},
-    [BASE + 99] = {"vase", "toggle"},
-    [BASE + 100] = {"goronade", "toggle"},
-    [BASE + 101] = {"gravekey", "toggle"},
-    [BASE + 102] = {"chart", "toggle"},
-    [BASE + 103] = {"lavajuice", "toggle"},
-    [BASE + 104] = {"introduction", "toggle"},
-    [BASE + 105] = {"librarykey", "toggle"},
-    [BASE + 106] = {"d6keypast", "toggle"},
-    [BASE + 107] = {"d6keypresent", "toggle"},
-    [BASE + 108] = {"gloves", "toggle"},
-    [BASE + 109] = {"brisket", "toggle"},
-    [BASE + 110] = {"seedling", "toggle"},
-    [BASE + 111] = {"d8slate", "consumable"},
-    [BASE + 112] = {"eyeball", "toggle"},
-    [BASE + 113] = {"tuninut", "toggle"},
-    [BASE + 114] = {"repairednut", "toggle"},
-    [BASE + 115] = {"scale", "toggle"},
+    {"bombflower", "toggle"},
+    {"book", "toggle"},
+    {"emblem", "toggle"},
+    {"cheval", "toggle"},
+    {"crownkey", "toggle"},
+    {"powder", "toggle"},
+    {"vase", "toggle"},
+    {"goronade", "toggle"},
+    {"gravekey", "toggle"},
+    {"chart", "toggle"},
+    {"lavajuice", "toggle"},
+    {"introduction", "toggle"},
+    {"librarykey", "toggle"},
+    {"d6keypast", "toggle"},
+    {"d6keypresent", "toggle"},
+    {"gloves", "toggle"},
+    {"brisket", "toggle"},
+    {"seedling", "toggle"},
+    {"d8slate", "consumable"},
+    {"eyeball", "toggle"},
+    {"tuninut", "toggle"},
+    {"repairednut", "toggle"},
+    {"scale", "toggle"},
     -- RINGS
-    [BASE + 127] = {"ring_expert", "toggle"},
-    [BASE + 134] = {"ring_toss", "toggle"},
-    [BASE + 165] = {"ring_energy", "toggle"},
-    [BASE + 178] = {"ring_fist", "toggle"},
+    11,
+    {"ring_expert", "toggle"},
+    6,
+    {"ring_toss", "toggle"},
+    30,
+    {"ring_energy", "toggle"},
+    11,
+    {"ring_fist", "toggle"},
+    2,
     -- ESSENCE
-    [BASE + 180] = {"d1", "toggle"},
-    [BASE + 181] = {"d2", "toggle"},
-    [BASE + 182] = {"d3", "toggle"},
-    [BASE + 183] = {"d4", "toggle"},
-    [BASE + 184] = {"d5", "toggle"},
-    [BASE + 185] = {"d6", "toggle"},
-    [BASE + 186] = {"d7", "toggle"},
-    [BASE + 187] = {"d8", "toggle"},
+    {"d1", "toggle"},
+    {"d2", "toggle"},
+    {"d3", "toggle"},
+    {"d4", "toggle"},
+    {"d5", "toggle"},
+    {"d6", "toggle"},
+    {"d7", "toggle"},
+    {"d8", "toggle"}
 }
+
+function BuildItemMapping()
+    local item_mapping = {}
+    local currentIndex = BASE
+    for _, item in ipairs(ITEM_LISTING) do
+        if type(item) == "number" then
+            currentIndex = currentIndex + item
+        else
+            item_mapping[currentIndex] = item
+            currentIndex = currentIndex + 1
+        end
+    end
+
+    return item_mapping
+end
+
+ITEM_MAPPING = BuildItemMapping()
